1452b7fbc26caca112a9c12fd4c2280237e9ea84
gfsanluis78 <71910385+gfsanluis78@users.noreply.github.com>
2021-09-23 00:29:59 -0300

    Se retira el toast inflate

--- app/src/main/java/com/farias/fariastpintegrador/MainActivity.java
+++ app/src/main/java/com/farias/fariastpintegrador/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        Toast.makeText(this, "Infla menu", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Infla menu", Toast.LENGTH_SHORT).show();
         return true;
     }
 
